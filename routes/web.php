51fd8d9d088c250efdc2c4fc18621424d65168df
zydnrbrn <zidanreborn@gmail.com>
2024-06-06 16:59:38 +0700

    chore: Update dependencies and configuration files
    The commit updates the dependencies in `composer.json` and `package.json` files. It also modifies the configuration in `vite.config.js`.

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Index');
 });
 
 Route::get('/dashboard', function () {
@@ -24,4 +19,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
