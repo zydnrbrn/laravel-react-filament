ac9dd2b01168d1c2cfc8b4f67cb18ef4a3378053
zydnrbrn <zidanreborn@gmail.com>
2024-06-08 23:32:15 +0700

    refactor: update schema database

--- database/migrations/2024_06_07_070737_create_products_table.php
+++ database/migrations/2024_06_07_070737_create_products_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('merchant_id')->constrained('merchants')->onDelete('cascade');
             $table->string('name');
             $table->text('description');
             $table->decimal('price', 10, 2);
